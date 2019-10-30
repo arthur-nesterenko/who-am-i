16685649271d83aa4eb7ff5b7205527c60de9af7
Arthur <nesterenko19@gmail.com>
2019-10-30 17:34:17 +0200

    :wrench: config: setup gatsby configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,17 @@
+const config = require('./content/meta/config');
+
 module.exports = {
   siteMetadata: {
-    title: `My Personal blg`,
-    description: `This is me`,
-    author: `@arthurnstr`,
+    title: config.siteTitle,
+    description: config.siteDescription,
+    siteUrl: config.siteUrl,
+    pathPrefix: config.pathPrefix,
+    authorSocialLinks: config.authorSocialLinks,
+    author: config.authorName,
+    contactEmail: config.contactEmail,
+    authorTwitterAccount: config.authorTwitterAccount,
   },
   plugins: [
-    'gatsby-plugin-emotion',
     {
       resolve: `gatsby-plugin-alias-imports`,
       options: {
@@ -16,12 +22,11 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-layout`,
+      resolve: 'gatsby-plugin-transition-link',
       options: {
-        component: require.resolve(`./src/layouts/main.jsx`),
+        layout: require.resolve(`./src/layouts/index.jsx`),
       },
     },
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -29,23 +34,139 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/content/posts/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/pages/`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-plugin-sharp`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+              backgroundColor: 'transparent',
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 2em`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: 'gatsby-remark-external-links',
+      options: {
+        target: '_blank',
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        name: config.manifestName,
+        short_name: config.manifestShortName,
+        start_url: config.manifestStartUrl,
+        background_color: config.manifestBackgroundColor,
+        theme_color: config.manifestThemeColor,
+        display: config.manifestDisplay,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  limit: 1000,
+                  sort: { order: DESC, fields: [fields___prefix] },
+                  filter: { id: { regex: "//posts//" } }
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields {
+                        slug
+                      }
+                      frontmatter {
+                        title
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Montserrat`,
+            variants: [`400`, `500`, `500i`, `600`, `700`, `900`, `900i`],
+            subsets: [`cyrillic`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`400`, `700`],
+          },
+        ],
+      },
+    },
   ],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,74 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const _ = require('lodash');
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: `posts`,
+      trailingSlash: false,
+    });
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+    createNodeField({
+      node,
+      name: 'directoryName',
+      value: path.basename(path.dirname(_.get(node, 'fileAbsolutePath'))),
+    });
+  }
+};
+//
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  return new Promise((resolve, reject) => {
+    const postTemplate = path.resolve('./src/templates/post.jsx');
+    resolve(
+      graphql(
+        `
+          {
+            allMarkdownRemark(
+              filter: { fileAbsolutePath: { regex: "//posts//" } }
+              limit: 1000
+            ) {
+              edges {
+                node {
+                  id
+                  fields {
+                    slug
+                  }
+                }
+              }
+            }
+          }
+        `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors);
+          reject(result.errors);
+        }
+
+        // Create posts and pages.
+        result.data.allMarkdownRemark.edges.forEach(edge => {
+          const slug = edge.node.fields.slug;
+          const id = edge.node.id;
+
+          createPage({
+            path: `/blog${slug}`,
+            component: postTemplate,
+            context: {
+              slug: slug,
+              id,
+            },
+          });
+        });
+      })
+    );
+  });
+};
