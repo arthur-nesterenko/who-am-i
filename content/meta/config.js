7ddbba21cadaa3ba05a5994d03dbcd6cb2fef5f1
Arthur <nesterenko19@gmail.com>
2019-11-03 15:51:33 +0200

    :bug: bug: remove site config from .gitignore

---
+++ content/meta/config.js
@@ -0,0 +1,54 @@
+const colors = require("../../src/assets/theme/colors");
+
+module.exports = {
+  siteTitle: "Arthur's Personal Blog.",
+  shortSiteTitle: "Arthur's Blog.", // <title> ending for posts and pages
+  siteDescription: "It's the place where I share my thoughts.",
+  siteUrl: 'http://0.0.0.0',
+  pathPrefix: "/",
+  siteImage: "",
+  siteLanguage: "en",
+  // author
+  authorName: "arthur nesterenko",
+  authorTwitterAccount: "arthurnstr",
+  // info
+  infoTitle: "arthur nesterenko",
+  infoTitleNote: "personal blog",
+  // manifest.json
+  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
+  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestStartUrl: "/",
+  manifestBackgroundColor: colors.primary,
+  manifestThemeColor: colors.primary,
+  manifestDisplay: "standalone",
+  // contact
+  contactEmail: "arthur.nesterenko.dev@gmail.com",
+  // social
+  authorSocialLinks: [
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com',
+      nickname: '@arthurnstr',
+    },
+    {
+      nickname: 'arthur-nesterenko',
+      url: 'https://github.com/',
+      name: 'Github',
+    },
+    {
+      url: 'https://www.linkedin.com/in/',
+      nickname: 'arthur-nesterenko',
+      name: 'Linkedin',
+    },
+    {
+      name: 'StackOverflow',
+      url: 'https://stackoverflow.com/users/',
+      nickname: '5380164/arthur',
+    },
+    {
+      name: 'Envelope',
+      nickname: 'arthur.nesterenko.dev@gmail.com',
+      url: 'mailto:',
+    },
+  ]
+};
