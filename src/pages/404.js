566ac488299c8604443bca2340511076b543782f
Arthur <nesterenko19@gmail.com>
2019-11-03 14:26:24 +0200

    :lipstick: update: UI 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import SEO from '../components/seo';
+import Container from '@/components/container';
+import PageLayout from '@/layouts/page-layout';
+import { css } from '@emotion/core';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+
+const centerText = props => css`
+  text-align: center;
+  color: ${props.colors.white};
+`;
+
+const container = css`
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const title = css`
+  font-size: 2.3rem;
+`;
+
+const link = props => css`
+  color: ${props.colors.white};
+  text-transform: uppercase;
+`;
 
 const NotFoundPage = () => (
-  <div>
+  <PageLayout withHeader={false}>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+    <Container css={container}>
+      <div css={centerText}>
+        <h1 css={title}>NOT FOUND 🤷🏻‍♂️</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.😔</p>
+        <AniLink css={link} to="/" fade>
+          Go home
+        </AniLink>
+      </div>
+    </Container>
+  </PageLayout>
 );
 
 export default NotFoundPage;
